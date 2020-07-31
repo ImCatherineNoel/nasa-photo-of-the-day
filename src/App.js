39d4f512eadb2b4ab7a9678fbe8d6b9fd3e69759
Catherine Tucker <ImCatherineNoel@gmail.com>
2020-07-30 17:35:51 -0700

    cleaned up some code

--- src/App.js
+++ src/App.js
@@ -1,45 +1,47 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
-import axios from 'axios';
-import Header from './Comps/header.js';
-import Main from './Comps/main.js';
-import Footer from './Comps/footer.js'
-import styled from 'styled-components';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import Header from "./Comps/header.js";
+import Main from "./Comps/main.js";
+import Footer from "./Comps/footer.js";
+import styled from "styled-components";
 
 const Wrapper = styled.div`
-    // border: 5px solid red;
-    max-width: 800px;
-    margin: 0px auto;
-    background-color:AntiqueWhite; 
-    padding: 0;
-    border-bottom-left-radius: 25px;
-    border-bottom-right-radius: 25px;   
+	// border: 5px solid red;
+	max-width: 800px;
+	margin: 0px auto;
+	background-color: AntiqueWhite;
+	padding: 0;
+	border-bottom-left-radius: 25px;
+	border-bottom-right-radius: 25px;
 `;
 
-
 function App() {
+	const [data, setData] = useState("");
 
-    const [data, setData] = useState('');
+	useEffect(() => {
+		axios.get(
+			"https://api.nasa.gov/planetary/apod?api_key=JcscI1lYExVXYgatImbIRf0G728iv6lXTqNeEqge&date=2018-12-18"
+		)
+			.then((response) => {
+				setData(response.data);
+				console.log(data);
+			})
+			.catch((error) => console.log("Error!", error));
+	}, []);
 
-    useEffect( () =>{
-    axios.get('https://api.nasa.gov/planetary/apod?api_key=JcscI1lYExVXYgatImbIRf0G728iv6lXTqNeEqge&date=2018-12-18')
-    .then(response => {
-        
-        setData(response.data);
-        console.log(data);
-    })
-    .catch(error => console.log('Error!', error));
-    }, []); 
-    
-
-return (
-	<Wrapper> 
-        < Header title={data.title}/>
-        < Main url={data.url} title={data.title} date={data.date} copyright={data.copyright} explanation={data.explanation}/>           
-        < Footer hdurl={data.hdurl} />   
-    </Wrapper>
-    
-  );
-
-};
+	return (
+		<Wrapper>
+			<Header title={data.title} />
+			<Main
+				url={data.url}
+				title={data.title}
+				date={data.date}
+				copyright={data.copyright}
+				explanation={data.explanation}
+			/>
+			<Footer hdurl={data.hdurl} />
+		</Wrapper>
+	);
+}
 export default App;
--- src/Comps/header.js
+++ src/Comps/header.js
@@ -1,21 +1,16 @@
-import React from 'react';
-import styled from 'styled-components';
-
+import React from "react";
+import styled from "styled-components";
 
 const Title = styled.h1`
-  color:white;
-  font-size: 2em;
-  text-align: center;
-  margin: 0px auto;
-  // border: 5px solid DarkOrange;
-  background-color: black;
+	color: white;
+	font-size: 2em;
+	text-align: center;
+	margin: 0px auto;
+	// border: 5px solid DarkOrange;
+	background-color: black;
 `;
 
 function Header(props) {
-  return (   
-     <Title>
-          {props.title}
-     </Title>
-  );
-};
+	return <Title>{props.title}</Title>;
+}
 export default Header;
